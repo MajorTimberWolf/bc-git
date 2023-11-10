b01573a8f8d8ff97be0f05145dc8bcf707f7dfc5
MajorTimberWolf <ak0929@srmist.edu.in>
2023-11-11 01:59:09 +0530

    feat: define lru function Defined LRU Function for cache optimization
    BREAKING CHANGE
    bump version of jax module for function to run in prod build

---
+++ python-commits/vizor/pythia.py
@@ -0,0 +1,10 @@
+import functools
+
+from vizier._src.jax.optimizers.core import LossFunction
+from vizier._src.jax.optimizers.core import Optimizer
+
+@functools.lru_cache
+def default_optimizer(maxiter: int = 50) -> Optimizer:
+  """Default optimizer and random restarts that work okay for most cases."""
+  # NOTE: Production algorithms are recommended to stay away from using this.
+  return JaxoptScipyLbfgsB(LbfgsBOptions(maxiter=maxiter, best_n=None))
