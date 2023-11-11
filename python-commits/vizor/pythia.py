5a91ed6fa126080ff5041354ecc4abf0317a8332
MajorTimberWolf <ak0929@srmist.edu.in>
2023-11-11 13:23:16 +0530

    refactor: changed formatting

--- python-commits/vizor/pythia.py
+++ python-commits/vizor/pythia.py
@@ -7,4 +7,6 @@ from vizier._src.jax.optimizers.core import Optimizer
 def default_optimizer(maxiter: int = 50) -> Optimizer:
   """Default optimizer and random restarts that work okay for most cases."""
   # NOTE: Production algorithms are recommended to stay away from using this.
-  return JaxoptScipyLbfgsB(LbfgsBOptions(maxiter=maxiter, best_n=None)) #IGNORE: ignore error lines
+  return JaxoptScipyLbfgsB(
+    LbfgsBOptions(maxiter=maxiter, best_n=None)
+) #IGNORE: ignore error lines
